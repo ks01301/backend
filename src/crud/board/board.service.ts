652baad6b71086911e4619c33923c3556b7bd96a
aa <aa@adf.com>
2024-03-15 01:15:33 +0900

    status code return add

--- src/crud/board/board.service.ts
+++ src/crud/board/board.service.ts
@@ -11,27 +11,35 @@ export class BoardService {
   private boardRepository: Repository<BoardEntity>;
   constructor(private memberService: MemberService) {}
 
-  list(boardId: number) {
-    console.log(boardId);
+  async list(boardId: number) {
+    const result = await this.boardRepository.findOne({ where: { boardId } });
+    console.log(result);
 
-    if (boardId === null || boardId === undefined) {
-      return this.boardRepository.find();
-    } else {
-      return this.boardRepository.findOne({ where: { boardId } });
-    }
+    if (result === null || result === undefined)
+      return { status: 200, message: '없는 게시판입니다' };
+    else return { status: 200, message: '게시판 불러오기 성공', data: result };
   }
 
   async write(board: CreateBoardDto) {
     const sendBoard: any = { ...board };
     sendBoard.date = new Date();
     sendBoard.editDate = new Date();
-    const result = await this.boardRepository.save(sendBoard);
+    // const result = await this.boardRepository.save(sendBoard);
 
-    return {
-      statusCode: 200,
-      message: '게시글 작성 성공',
-      data: result,
-    };
+    try {
+      const result = await this.boardRepository.save(sendBoard);
+      return {
+        status: 200,
+        message: '게시글 작성 성공',
+        data: result,
+      };
+    } catch (error) {
+      return {
+        status: 400,
+        message: '게시글 작성 실패',
+        data: error,
+      };
+    }
   }
 
   async update(boardId: number, board: UpdateBoardDto) {
@@ -49,8 +57,18 @@ export class BoardService {
     sendBoard.editDate = new Date();
 
     const result = await this.boardRepository.update({ boardId }, sendBoard);
-
-    return { statusCode: 200, message: '게시글 수정 완료', data: result };
+    console.log(result.affected);
+    if (result.affected !== 0)
+      return {
+        status: 200,
+        message: '게시글 수정 완료',
+        data: result,
+      };
+    else
+      return {
+        status: 400,
+        message: '존재하지 않는 게시글',
+      };
   }
 
   async delete(boardId: number) {
